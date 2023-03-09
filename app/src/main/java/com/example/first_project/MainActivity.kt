4cb6e64a60db88fe1dd204dd33a804d465d98906
Samuel Mejía <samuelmejiagir@gmail.com>
2023-03-08 22:05:55 -0500

    Uploading components and funcionalities

--- app/src/main/java/com/example/first_project/MainActivity.kt
+++ app/src/main/java/com/example/first_project/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.EditText
 import android.widget.Toast
 import android.widget.Button
+import android.widget.ToggleButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +19,16 @@ class MainActivity : AppCompatActivity() {
     private fun onClick(){
         val fieldTxt:EditText= findViewById(R.id.txtUserName)
         var name:String= fieldTxt.text.toString()
-        Toast.makeText(this,"Bienvenido $name",Toast.LENGTH_LONG).show()
+
+        val toggleBtn:ToggleButton= findViewById(R.id.toggleButton)
+        toggleBtn.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                Toast.makeText(this,"Bienvenido $name, \n toogle activo",Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this,"Bienvenido $name, \n toogle inactivo",Toast.LENGTH_LONG).show()
+            }
+        }
+
     }
 
 }
