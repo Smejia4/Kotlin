cf0d1dcd4295be80b3ce355515bd9d7d2d846996
Samuel Mejía <samuelmejiagir@gmail.com>
2023-03-09 08:58:03 -0500

    Uploading more funcionalities

--- app/src/main/java/com/example/first_project/MainActivity.kt
+++ app/src/main/java/com/example/first_project/MainActivity.kt
@@ -2,10 +2,7 @@ package com.example.first_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
-import android.widget.Toast
-import android.widget.Button
-import android.widget.ToggleButton
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,15 +17,56 @@ class MainActivity : AppCompatActivity() {
         val fieldTxt:EditText= findViewById(R.id.txtUserName)
         var name:String= fieldTxt.text.toString()
 
-        val toggleBtn:ToggleButton= findViewById(R.id.toggleButton)
+        var toggleBtn:ToggleButton= findViewById(R.id.toggleButton)
+        var statusToggle:String = ""
         toggleBtn.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
-                Toast.makeText(this,"Bienvenido $name, \n toogle activo",Toast.LENGTH_LONG).show()
+                statusToggle = "Toogle activo"
             } else {
-                Toast.makeText(this,"Bienvenido $name, \n toogle inactivo",Toast.LENGTH_LONG).show()
+                statusToggle = "Toogle inactivo"
             }
         }
 
+        var switchBtn:Switch = findViewById(R.id.switch1)
+        var statusSwitch:String = ""
+            switchBtn.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                   statusSwitch= "Switch activo"
+                } else {
+                    statusSwitch = "Switch inactivo"
+                }
+            }
+
+        var checkOne:CheckBox =findViewById(R.id.checkBox)
+        var checkTwo:CheckBox =findViewById(R.id.checkBox2)
+        var checkResult: String = ""
+        if (checkOne.isChecked){
+            checkResult += "Check 1 activo "
+        }else{
+            checkResult += "Check 1 inactivo "
+        }
+        if (checkTwo.isChecked) {
+            checkResult += "Check 2 activo "
+        }else{
+            checkResult += "Check 2 inactivo "
+        }
+
+        var radioOne:RadioButton =findViewById(R.id.radioButton)
+        var radioTwo:RadioButton =findViewById(R.id.radioButton2)
+        var radioResult: String = ""
+        if (radioOne.isChecked){
+            radioResult += "opcion 1"
+        }else{
+            radioResult += "error"
+        }
+
+        if(radioTwo.isChecked) {
+            radioResult += "opcion 2 "
+        }else{
+            radioResult += "error"
+        }
+
+        Toast.makeText(this,"$name \n $statusToggle \n $statusSwitch \n $checkResult \n $radioResult", Toast.LENGTH_LONG).show()
     }
 
 }
